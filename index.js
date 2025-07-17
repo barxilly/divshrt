3eb254593a910e3cc3c604bbdb340d8b24ed3f21
Ben Smith <ben@barxsmith.sbs>
2025-07-17 16:58:14 +0100

    7

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ for (const [key, value] of Object.entries(urls)) {
   });
 }
 
-app.post('/add-url', express.json(), (req, res) => {
-  const jsonData = req.body;
+app.post('/add-url', express.json(),async (req, res) => {
+  const jsonData = await req.body;
   const short = jsonData[0]
   const long = jsonData[1];
   if (!short || !long) {
