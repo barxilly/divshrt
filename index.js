4bcfe3f241e660b5c08a1bdf601a35833e93cc30
Ben Smith <ben@barxsmith.sbs>
2025-07-17 20:36:13 +0100

    20

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ env.config();
 const app = express();
 const port = 4254;
 
+// Add CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
